e2113e846e01443ae0df15f966c2c279e25b614b
Carolina Carvalho <carolina.dossantos0806@gmail.com>
2024-06-25 21:16:57 -0300

    day 1

---
+++ day-1/main.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator")
+
+city = input("What's the name of the city you grew up in?\n")
+print(city)
+
+pet_name = input("What's your pets name?\n")
+print(pet_name)
+
+print("Your band name could be " + city + " " + pet_name)
